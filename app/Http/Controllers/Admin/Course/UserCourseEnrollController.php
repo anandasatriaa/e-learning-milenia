9a9080ec9cd72c024cde313ddc7e0310142bf8f9
Ananda Satria <anandasatriaf6@gmail.com>
2024-12-03 22:24:00 +0700

    fix: button unenroll user

--- app/Http/Controllers/Admin/Course/UserCourseEnrollController.php
+++ app/Http/Controllers/Admin/Course/UserCourseEnrollController.php
@@ -30,6 +30,33 @@ class UserCourseEnrollController extends Controller
         }
     }
 
+    public function destroyUser($course_id, $user_id)
+    {
+        try {
+            DB::beginTransaction();
+            $enroll = UserCourseEnroll::where('course_id', $course_id)
+                                        ->where('user_id', $user_id)
+                                        ->firstOrFail();
+            $enroll->delete();
+    
+            DB::commit();
+            return redirect()->route('admin.course.modul.index', $course_id)->with([
+                'success' => [
+                    'title' => 'Sukses',
+                    'message' => 'Berhasil menghapus data!'
+                ]
+            ]);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return redirect()->back()->with([
+                'error' => [
+                    'title' => 'Error!',
+                    'message' => $th->getMessage()
+                ]
+            ]);
+        }
+    }
+
     public function userCourseRollout(Request $request, $course_id)
     {
         try {
